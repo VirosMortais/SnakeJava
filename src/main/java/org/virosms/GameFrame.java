7f30e0e099c6264c1b7ea055602356869936b5c3
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-21 20:56:49 +0200

    Documented

--- src/main/java/org/virosms/GameFrame.java
+++ src/main/java/org/virosms/GameFrame.java
@@ -3,9 +3,27 @@ package org.virosms;
 import lombok.Data;
 
 import javax.swing.JFrame;
-@Data
+/**
+ * The GameFrame class is a JFrame that contains a GamePanel.
+ *
+ * @see GamePanel
+ * @see JFrame
+ * @see SnakeGame
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author VirosMs
+ *
+ */
 public class GameFrame extends JFrame{
-
+    /**
+     * The GameFrame constructor.
+     * <p> This constructor creates a new GamePanel and adds it to the GameFrame. </p>
+     * <p> It also sets the title of the GameFrame to "Snake", sets the default close operation to exit on close,
+     * sets the GameFrame to not be resizable, sets the GameFrame to be visible, and sets the location of the GameFrame
+     * to the center of the screen. </p>
+     *
+     */
     public GameFrame(){
         this.add(new GamePanel());
         this.setTitle("Snake");
--- src/main/java/org/virosms/SnakeGame.java
+++ src/main/java/org/virosms/SnakeGame.java
@@ -1,6 +1,9 @@
 package org.virosms;
 
-
+/**
+ * The SnakeGame class is the main class of the Snake game.
+ * <p> This class contains the main method that creates a new GameFrame. </p>
+ */
 public class SnakeGame {
     public static void main(String[] args) {
 
